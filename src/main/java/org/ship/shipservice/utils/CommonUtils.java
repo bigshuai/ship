10e87004f1a8ec126c988e0415a21e5679c5b17a
zhaohfup <zhaohfup@gmail.com>
2015-09-05 21:32:53 +0800

    发短信

--- src/main/java/org/ship/shipservice/utils/CommonUtils.java
+++ src/main/java/org/ship/shipservice/utils/CommonUtils.java
@@ -31,7 +31,7 @@ import com.alibaba.fastjson.JSON;
 
 public class CommonUtils {
 	 static char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8','9', 'a', 'b', 'c', 'd', 'e', 'f' };
-	/**
+	 /**
 	 * 随机生成验证码
 	 * 
 	 * @param numberFlag
@@ -72,21 +72,21 @@ public class CommonUtils {
 	 * @param phone
 	 * @return
 	 */
-	public static String sendMessage(Integer phone, String content) {
+	public static String sendMessage() {
 		String inputline = "";
 		// 发送内容
 		String sign = "签名";
 		// 创建StringBuffer对象用来操作字符串
 		StringBuffer sb = new StringBuffer(
-				"http://sms.1xinxi.cn/asmx/smsservice.aspx?");
+				"http://web.1xinxi.cn/asmx/smsservice.aspx?");
 		// 向StringBuffer追加用户名
-		sb.append("name=登录名、手机或qq");
+		sb.append("name=chenminmin008@126.com");
 		// 向StringBuffer追加密码（登陆网页版，在管理中心--基本资料--接口密码，是28位的）
-		sb.append("&pwd=fa246d0262c3925617b0c72bb20eeb");
+		sb.append("&pwd=B57C526FC74B32DEA9A9FBE7ED7A");
 		// 向StringBuffer追加手机号码
-		sb.append("&mobile=13339196131,15375379376");
+		sb.append("&mobile=18516293301");
 		// 向StringBuffer追加消息内容转URL标准码
-		sb.append("&content=" + URLEncoder.encode(content));
+		sb.append("&content=" + URLEncoder.encode("测试"));
 		// 追加发送时间，可为空，为空为及时发送
 		sb.append("&stime=");
 		// 加签名
@@ -274,8 +274,58 @@ public class CommonUtils {
 	}
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		String f = "%E7%8E%8B%E6%B4%AA%E4%BE%A0";
-		System.out.println(URLEncoder.encode("工商银行", "GBK"));
-		System.out.println(decode(f));
+		//发送内容
+				String content = "尊敬的航运宝用户，你的验证码为 123456"; 
+				String sign="航运宝";
+				
+				// 创建StringBuffer对象用来操作字符串
+				StringBuffer sb = new StringBuffer("http://web.1xinxi.cn/asmx/smsservice.aspx?");
+
+				// 向StringBuffer追加用户名
+				sb.append("name=chenminmin008@126.com");
+
+				// 向StringBuffer追加密码（登陆网页版，在管理中心--基本资料--接口密码，是28位的）
+				sb.append("&pwd=B57C526FC74B32DEA9A9FBE7ED7A");
+
+				// 向StringBuffer追加手机号码
+				sb.append("&mobile=18516293301");
+
+				// 向StringBuffer追加消息内容转URL标准码
+				sb.append("&content="+URLEncoder.encode(content));
+				
+				//追加发送时间，可为空，为空为及时发送
+				sb.append("&stime=");
+				
+				//加签名
+				sb.append("&sign="+URLEncoder.encode(sign));
+				
+				//type为固定值pt  extno为扩展码，必须为数字 可为空
+				sb.append("&type=pt&extno=");
+				// 创建url对象
+				URL url;
+				try {
+					url = new URL(sb.toString());
+					// 打开url连接
+					HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+					// 设置url请求方式 ‘get’ 或者 ‘post’
+					connection.setRequestMethod("POST");
+
+					// 发送
+					BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+					// 返回发送结果
+					String inputline = in.readLine();
+					System.out.println(inputline);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				
+
+				// 返回结果为‘0，20140009090990,1，提交成功’ 发送成功   具体见说明文档
+				
+
 	}
 }
